a57f199ef67b22d85f01a3803356d896bf0414a4
Roman Kontsevoy <3844320@gmail.com>
2019-01-25 16:50:58 +0200

    canvas banner

--- src/script.js
+++ src/script.js
@@ -28,3 +28,39 @@ function greet(name) {
 }
 
 greet("roma");
+
+let canv = document.getElementById('canv');
+const ctx = canv.getContext('2d');
+
+canv.width = 1200;
+canv.height = 200;
+
+let grd = ctx.createLinearGradient(0, 0, canv.width, canv.height);
+function makeBackground () {
+    grd.addColorStop(0, "red");
+    grd.addColorStop(0.16, "orange");
+    grd.addColorStop(0.32, "yellow");
+    grd.addColorStop(0.48, "green");
+    grd.addColorStop(0.64, "lightblue");
+    grd.addColorStop(0.80, "blue");
+    grd.addColorStop(1, "violet");
+    ctx.fillStyle = grd;
+    ctx.fillRect(0, 0, canv.width, canv.height);
+}
+
+function drawText() {
+    ctx.beginPath();
+    ctx.font = "66px Verdana";
+    ctx.strokeStyle = "red";
+    ctx.fillStyle = "blue";
+    ctx.textAlign = "center";
+    ctx.textBaseline="middle";
+    ctx.lineWidth = 4;
+    ctx.strokeText("Тестовый баннер", canv.width/2, canv.height/2);
+    ctx.fillText("Тестовый баннер", canv.width/2, canv.height/2);
+    let text = ctx.measureText("Тестовый баннер");
+    console.log(text);
+}
+
+makeBackground();
+drawText();
